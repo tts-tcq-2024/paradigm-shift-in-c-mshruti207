1da67de4574bbda2ced543153e9afb7f5f0c4446
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-08-26 07:46:43 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,42 +1,21 @@
 #include <stdio.h>
 #include <assert.h>
-void tempok(float temp);
-void socok(float soc);
-void chrgRteok(float chrgRte);
-int batteryIsOk(float temperature, float soc, float chargeRate) 
-{
-    tempok(temperature);
-    socok(soc);
-    chrgRteok(chargeRate);
-    return 1;
-}
 
-void tempok(float temp)
-{
-    if (temp < 0 || temp > 45) 
-    {
-        printf("Temperature out of range!\n");
-    } 
-}
-
-void socok(float soc)
-{
-if(soc < 20 || soc > 80) 
-{
+int batteryIsOk(float temperature, float soc, float chargeRate) {
+  if(temperature < 0 || temperature > 45) {
+    printf("Temperature out of range!\n");
+    return 0;
+  } else if(soc < 20 || soc > 80) {
     printf("State of Charge out of range!\n");
-}
-}
-  
-void chrgRteok(float chrgRte)
-{
-    if(chrgRte > 0.8) 
-    {
+    return 0;
+  } else if(chargeRate > 0.8) {
     printf("Charge Rate out of range!\n");
-}
+    return 0;
+  }
+  return 1;
 }
 
 int main() {
   assert(batteryIsOk(25, 70, 0.7));
   assert(!batteryIsOk(50, 85, 0));
 }
- 
