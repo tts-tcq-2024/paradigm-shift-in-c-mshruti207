975d4e83edef9dd20a36973fc3794433c7aa5557
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 10:25:31 +0530

    Update WarningConfiguration.c

--- WarningConfiguration.c
+++ WarningConfiguration.c
@@ -2,14 +2,3 @@
 
 WarningConfiguration warningConfiguration = {1, 1, 1}; // Enable all warnings by default
 
-/*void EnableTemperatureWarning(int enable_disable) {
-    warningConfiguration.temperatureWarningEnabled = enable_disable;  //Set 0 to Disbale warning and 1 to Enable Warning
-}
-
-void EnableSocWarning(int enable_disable) {
-    warningConfiguration.socWarningEnabled = enable_disable;
-}
-
-void EnableChargeRateWarning(int config) {
-    warningConfiguration.chargeRateWarningEnabled = enable_disable;
-}*/
