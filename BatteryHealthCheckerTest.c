ec741d9b85102e0a6ae5e9136e48220943b2f422
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 10:12:23 +0530

    Update BatteryHealthCheckerTest.c

--- BatteryHealthCheckerTest.c
+++ BatteryHealthCheckerTest.c
@@ -7,23 +7,23 @@ void TestBatteryHealthNormal() {
 }
 
 void TestBatteryHealthTemperature() {
-    assert(!BatteryIsOk(50, 70, 0.7)); // Temperature too high
-    assert(!BatteryIsOk(-1, 70, 0.7)); // Temperature too low
-    assert(BatteryIsOk(1, 70, 0.7)); // Temperature warning low
-    assert(BatteryIsOk(44, 70, 0.7)); // Temperature warning high
+    ASSERT_EQ(BatteryIsOk(50, 70, 0.7)); // Temperature too high
+    ASSERT_EQ(BatteryIsOk(-1, 70, 0.7)); // Temperature too low
+    ASSERT_EQ(BatteryIsOk(1, 70, 0.7)); // Temperature warning low
+    ASSERT_EQ(BatteryIsOk(44, 70, 0.7)); // Temperature warning high
 }
 
 void TestBatteryHealthSoc() {
-    assert(!BatteryIsOk(25, 85, 0.7)); // SOC too high
-    assert(!BatteryIsOk(25, 15, 0.7)); // SOC too low
-    assert(BatteryIsOk(25, 21, 0.7)); // SOC warning low
-    assert(BatteryIsOk(25, 76, 0.7)); // SOC warning high
+    ASSERT_EQ(BatteryIsOk(25, 85, 0.7)); // SOC too high
+    ASSERT_EQ(BatteryIsOk(25, 15, 0.7)); // SOC too low
+    ASSERT_EQ(BatteryIsOk(25, 21, 0.7)); // SOC warning low
+    ASSERT_EQ(BatteryIsOk(25, 76, 0.7)); // SOC warning high
 }
 
 void TestBatteryHealthChargeRate() {
-    assert(!BatteryIsOk(25, 70, 0.9)); // Charge rate too high
-    assert(BatteryIsOk(25, 70, 0.1)); // Charge rate normal
-    assert(BatteryIsOk(25, 70, 0.76)); // Charge rate warning high
+    ASSERT_EQ(BatteryIsOk(25, 70, 0.9)); // Charge rate too high
+    ASSERT_EQ(BatteryIsOk(25, 70, 0.1)); // Charge rate normal
+    ASSERT_EQ(BatteryIsOk(25, 70, 0.76)); // Charge rate warning high
 }
 
 void TestBatteryHealth() {
