b232aad357e1bd6c6b1a0d35548f68c0d011bc0c
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 07:57:44 +0530

    Update Param_Checker.h

--- Param_Checker.h
+++ Param_Checker.h
@@ -2,14 +2,14 @@
 #define PARAM_CHECKER_H
 
 void PrintWarning(const char* parameter, const char* condition);
-void PrintBreach(const char* parameter, const char* condition);
-int IsBelowLowerLimit(float value, float lowerLimit);
-int IsAboveUpperLimit(float value, float upperLimit);
+void PrintMessage(const char* parameter, const char* condition);
+int IsOutOfRange(float value, float lowerLimit, float upperLimit);
 int IsApproachingLowerLimit(float value, float lowerLimit, float warningTolerance);
 int IsApproachingUpperLimit(float value, float upperLimit, float warningTolerance);
-void CheckLowerLimitBreach(float value, float lowerLimit, const char* parameter);
+void CheckOutofRange(float value, float lowerLimit,float upperLimit, const char* parameter);
 void CheckLowerLimitWarning(float value, float lowerLimit, float warningTolerance, const char* parameter);
-int CheckLowerLimit(float value, float lowerLimit, float warningTolerance, const char* parameter, int warningEnabled);
+void CheckUpperLimitWarning(float value, float upperLimit, float warningTolerance, const char* parameter);
+int CheckRangeLimit(float value, float lowerLimit,float upperLimit, float warningTolerance, const char* parameter, int warningEnabled) {
 int IsWithinRange(float value, float lowerLimit, float upperLimit, float warningTolerance, const char* parameter, int warningEnabled);
 
 #endif // PARAM_CHECKER_H
