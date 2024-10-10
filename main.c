8e53ea71a1ea9645026dda548c7ede1d9e44e0f2
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 10:46:31 +0530

    Update main.c

--- main.c
+++ main.c
@@ -7,7 +7,6 @@ int main() {
     assert(BatteryIsOk(2, 22, 0.1));    // Values approaching lower range limit
     assert(BatteryIsOk(44, 79, 0.79));  // Values approaching upper range limit
     assert(BatteryIsOk(-1, 19, 0.81)); // All parameters out of range
-
-    printf("All test cases passed!\n");
+    printf("Test cases passed!\n");
     return 0;
 }
