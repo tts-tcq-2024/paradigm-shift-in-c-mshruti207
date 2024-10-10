229c8c4d6e1d5a9f9a12156a2581879e3687a723
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 09:51:01 +0530

    Update BatteryChecker.h

--- BatteryChecker.h
+++ BatteryChecker.h
@@ -1,9 +1,9 @@
 #ifndef BATTERY_CHECKER_H
 #define BATTERY_CHECKER_H
 
-static int TemperatureIsOk(float temperature);
-static int SocIsOk(float soc);
-static int ChargeRateIsOk(float chargeRate);
-static int BatteryIsOk(float temperature, float soc, float chargeRate);
+int TemperatureIsOk(float temperature);
+int SocIsOk(float soc);
+ int ChargeRateIsOk(float chargeRate);
+ int BatteryIsOk(float temperature, float soc, float chargeRate);
 
 #endif // BATTERY_CHECKER_H
