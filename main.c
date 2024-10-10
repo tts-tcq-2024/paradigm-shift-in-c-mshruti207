30466589515af9bbb76ccdb54807efa358270612
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 10:37:44 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
-#include "BatteryHealthCheckerTest.h"
+
 
 int main() {
-    TestBatteryHealth();
+    assert(batteryIsOk(25, 70, 0.7));   // All parameters in range
+    assert(!batteryIsOk(50, 85, 0.7));  // Temperature and SoC out of range
+    assert(batteryIsOk(2, 22, 0.1));    // Values approaching lower range limit
+    assert(batteryIsOk(44, 79, 0.79));  // Values approaching upper range limit
+    assert(!batteryIsOk(-1, 19, 0.81)); // All parameters out of range
+
+    printf("All test cases passed!\n");
     return 0;
 }
