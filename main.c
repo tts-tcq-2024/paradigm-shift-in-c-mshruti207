5ba56b52b7859b026a2d65172b67e3d0fdb618e3
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 11:20:03 +0530

    Update main.c

--- main.c
+++ main.c
@@ -6,6 +6,7 @@ int main() {
     EnableTemperatureWarning(1); // Enable temperature warnings
     EnableSocWarning(0);         // Disable SoC warnings
     EnableChargeRateWarning(1);  // Enable charge rate warnings
+
     assert(BatteryIsOk(25, 70, 0.7));   // All parameters in range
     assert(BatteryIsOk(50, 85, 0.7));  // Temperature and SoC out of range
     assert(BatteryIsOk(2, 22, 0.1));    // Values approaching lower range limit
