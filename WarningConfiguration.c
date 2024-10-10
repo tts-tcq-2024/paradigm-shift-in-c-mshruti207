794573e5f1714a78f5c6ea3c7e92c0d99ce3e775
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 10:47:48 +0530

    Update WarningConfiguration.c

--- WarningConfiguration.c
+++ WarningConfiguration.c
@@ -1,4 +1,4 @@
 #include "WarningConfiguration.h"
 
-WarningConfiguration warningConfiguration = {1, 1, 1}; // Enable all warnings by default
+WarningConfiguration warningConfiguration = {1, 1, 1}; // Enable all warnings 
 
