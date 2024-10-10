8731918dc943547e8d460266113c86b73eda8e55
mshruti207 <45748315+mshruti207@users.noreply.github.com>
2024-10-10 11:14:12 +0530

    Update WarningConfiguration.c

--- WarningConfiguration.c
+++ WarningConfiguration.c
@@ -1,6 +1,6 @@
 #include "WarningConfiguration.h"
 
-WarningConfiguration warningConfiguration = {1, 1, 1}; // Enable all warnings 
+ 
 void EnableTemperatureWarning(int enabled) {
     warningConfiguration.temperatureWarningEnabled = enabled;
 }
